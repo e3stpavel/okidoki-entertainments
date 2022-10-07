98f28eba52224d0e6288e6869aae26c25b44e332
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-10-07 12:58:10 +0300

    feat(oke-3): Added custom web page title according to app name in 'app.json'

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { expo } from '../../app.json'
 import App from './App'
 import './index.css'
+
+// set the title of the page
+document.title = expo.name
 
 // TODO: fix public path
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
