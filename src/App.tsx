a1f4c7e54d0f40be8548bb1d4acd7ef14a98080b
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-10-08 11:33:51 +0300

    fix(oke-3): Fixed image assets loading to React native's Image source

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import { Image, StyleSheet, Text, View } from 'react-native'
+import type { ImageSourcePropType } from 'react-native'
 import Comp from '~/comp'
-import * as v from '~/../assets/favicon.png'
+import favicon from '~/../assets/images/favicon.png'
 
 const styles = StyleSheet.create({
   container: {
@@ -10,16 +11,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 48,
+    height: 48,
+  },
 })
-
-console.log(v)
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Comp />
-      <Image source={v.default} />
+      <Image style={ styles.image } source={ favicon as ImageSourcePropType } />
       <StatusBar style="auto" />
     </View>
   )
